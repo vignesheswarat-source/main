7409fe06f898d98ccfd58dfa419ec109d0ab5510
Vignesh E <user@users-MacBook-Pro.local>
2026-04-26 22:12:44 -0400

    refreshing python

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hello world")
+
+message = "Vignesh"
+print(message.__len__())
+print(message.find("g"))
+print(message.upper())
+print(message.title())
+print(message.replace("Vignesh", "Vijay"))
+print(message.isalpha())
+print(message*10)
+
+name, age, weight = "Vignesh", 25, 70
+print(age)
+
+name = input("What is your name? ")
+print("Hello " + name)
